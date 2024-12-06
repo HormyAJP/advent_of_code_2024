dd20f48e3a6fd932b1959b6eb15104503651b12e
Andrew <andrew.parker>
2024-12-06 20:52:40 +0800

    Finishing day 4

--- 4.py
+++ 4.py
@@ -42,12 +42,39 @@ def find_all_xmas(word_search):
 
     return count
 
-        
+def find_all_mas_exes(word_search):        
+    count = 0
+    width = len(word_search[0])
+    height = len(word_search)
+    for col in range(1, width - 1):        
+        for row in range(1, height - 1):
+            if word_search[row][col] != "A":
+                continue
+            match word_search[row-1][col-1]:
+                case "M":
+                    if word_search[row+1][col+1] != "S":
+                        continue
+                case "S":
+                    if word_search[row+1][col+1] != "M":
+                        continue                    
+                case _:
+                    continue
+            match word_search[row-1][col+1]:
+                case "M":
+                    if word_search[row+1][col-1] != "S":
+                        continue
+                case "S":
+                    if word_search[row+1][col-1] != "M":
+                        continue
+                case _:
+                    continue                    
+            count += 1
+    return count
 
 # Read the input data from the file
 with open('input4.txt', 'r') as f:
     word_search = [line.strip() for line in f]
 
 # Count and print the number of occurrences of XMAS
-print(find_all_xmas(word_search))
+print(find_all_mas_exes(word_search))
 
